2a23cab5a306bd14341a38cd549e5c081b1d574c
leonhou <vleonhou@qq.com>
2017-08-30 03:29:12 +0000

    ...

--- c语言学习/my/weiyi.c
+++ c语言学习/my/weiyi.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <strings.h>
 #include <stdlib.h>
 
 
 //打印一个数字的二进制,10进制整数转换为二进制数，并打印出二进制
 void bitcode(int b)
 {
-    char *a;
-    // a = malloc(sizeof(char)*30);
-    int k;
+    printf("十进制数%d转化为二进制数为:",b);
+    int a[200];
+    int k,i=0;
     while(b/2)
     {
         k = b%2;
-        // printf("二进制数为：%d\n",a);
-        sprintf(a,"%d",k);
+        a[i++] = k;
         b /= 2;
         if( b/2 == 0 )
         {
-            sprintf(a,"%d",b);
+            a[i++] = b;
         }
-        // printf("b为：%d\n",b);
     }
-    printf("二进制数为：%s\n",a);
+    i--;
+
+    while(i>=0)
+    {
+        printf("%d",a[i--]);
+    }
+    printf("\n");
 }
 
 void resize(int nSize)
@@ -40,10 +45,9 @@ void resize(int nSize)
 int main()
 {
     int a[10] = {2,4,6,8,9,14,15,29,68,34};
-    // bitcode(a);
     for(int i=0;i<9;i++)
     {
-        resize(a[i]);
+        bitcode(a[i]);
     }
     return 0;
 }
